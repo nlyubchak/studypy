ee126296f9376cd3884e3d8fa80ed7684e5fa06e
nlyubchak <nlyubchak@gmail.com>
2020-05-17 22:35:14 +0300

    Text changed

--- hello/hello.py
+++ hello/hello.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-print( 'Hello, world!' ) #comment
-print( 'Hello, Home!' ) #comment
-print( 'Hello, Mam!' ) #comment
+print('Hello, world!')  # comment
+print('Hello, Home!')  # comment
+print('Hello, Mam!')  # comment
+print('Hello, Dad')  # comment
